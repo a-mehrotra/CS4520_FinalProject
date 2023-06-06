906349896ebb9f4cc2a6310ba89819bda87ea0a1
Aryan Mehrotra <amehrotra2001@gmail.com>
2023-06-06 18:06:38 -0400

    Initial commit

---
+++ FinalProject_CS4520/FinalProject_CS4520/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject_CS4520
+//
+//  Created by Aryan on 6/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
